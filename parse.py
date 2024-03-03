74c919f0576e8812469690d04d9f095cfc913ddc
xbonda06 <xbonda06@vutbr.cz>
2024-03-03 23:33:11 +0100

    feat: xml generator added

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 import sys
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 
 class ArgumentCheck:
@@ -20,15 +21,75 @@ class ArgumentCheck:
             sys.exit(0)
 
 
+class Instruction:
+    def __init__(self, order, name, args):
+        self.order = order
+        self.opcode = name
+        self.args = args
+
+
+class Argument:
+    def __init__(self, arg_type, arg_value):
+        self.type = arg_type
+        self.value = arg_value
+
+
+class XMLGenerator:
+    def __init__(self):
+        self.root = ET.Element("program", language="IPPcode24")
+
+    def create_xml_instruction(self, instruction):
+        xml_intruction = ET.Element("instruction", order=str(instruction.order), opcode=instruction.opcode)
+        for i, arg in enumerate(instruction.args, start=1):
+            arg_elem = ET.SubElement(xml_intruction, f"arg{i}", type=arg.type)
+            arg_elem.text = arg.value
+        self.root.append(xml_intruction)
+
+    def get_xml(self):
+        xml_str = ET.tostring(self.root, encoding="utf-8", method="xml", xml_declaration=True)
+        xml_str = minidom.parseString(xml_str).toprettyxml(indent="    ").encode("utf-8")
+        return xml_str
+
+
+def get_instruction_from_line(line, line_number):
+    line_without_comment = line.split("#", 1)
+    if len(line_without_comment) > 1:
+        line = line_without_comment[0]
+    parts_of_line = line.strip().split(maxsplit=1)
+    instruction_name = parts_of_line[0].upper()
+    if len(parts_of_line) > 1:
+        instruction_args = parts_of_line[1]
+    else:
+        instruction_args = ""
+    args = []
+    for arg in instruction_args.split():
+        if '@' in arg:
+            arg_type, arg_value = arg.split('@', 1)
+        else:
+            arg_type = "label"
+            arg_value = arg
+        args.append(Argument(arg_type, arg_value))
+    return Instruction(line_number, instruction_name, args)
+
+
 def main():
     arg_checker = ArgumentCheck(["parse.py", "--help"])
     arg_checker.check_args()
 
-    root = ET.Element("program", language="IPPcode24")
+    xml_gen = XMLGenerator()
 
-    xml_str = ET.tostring(root, encoding="utf-8", method="xml", xml_declaration=True)
+    line_number = 0
+    for line in sys.stdin:
+        if line.strip().startswith("#") or line.strip() == "":
+            continue
+        line_number += 1
+        instruction = get_instruction_from_line(line, line_number)
+        if instruction is not None:
+            xml_gen.create_xml_instruction(instruction)
 
+    xml_str = xml_gen.get_xml()
     sys.stdout.buffer.write(xml_str)
+
 
 if __name__ == "__main__":
     main()
